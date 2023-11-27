91d8dfe520f04d921dba4a3cf92b4fbdd9de4d6a
aekespong <andre.ekespong@gmail.com>
2023-11-27 16:55:45 +0100

    Added generate, removed reservations from User

--- db/models/user.py
+++ db/models/user.py
@@ -4,12 +4,13 @@ from sqlalchemy.orm import relationship
 from ..db_setup import Base
 from .mixins import Timestamp
 
+
 class User(Timestamp, Base):
-    __tablename__= "users"
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False)
     phone = Column(String(100), nullable=False)
     email = Column(String(100), nullable=False)
     unokod = Column(String(100), nullable=False)
-    reservation_uuid = Column(Integer, ForeignKey("reservations.id") , nullable=False)
+    # reservation_uuid = Column(Integer, ForeignKey("reservations.id"), nullable=False)
---
+++ generate.py
@@ -0,0 +1,51 @@
+from sqlalchemy import create_engine, Column, Integer, DateTime, func
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+from icecream import ic
+from faker import Faker
+
+# from core.config import settings
+from db.models.user import User
+
+Base = declarative_base()
+
+DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/noq"
+
+ic(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+def create_db_tables():
+    # Create the table
+    Base.metadata.create_all(bind=engine)
+
+
+# Define a function to get a database session
+def get_session():
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    return session
+
+
+def add_users():
+    faker = Faker("sv_SE")
+    session = get_session()
+
+    for i in range(4):
+        user = User(
+            name=faker.first_name(),
+            phone="0709-123123",
+            email=faker.email(),
+            unokod=""
+        )
+        session.add(user)
+        session.commit()
+        ic(user.id, user.name, "added")
+
+    session.close()
+
+
+if __name__ == "__main__":
+    create_db_tables()
+    add_users()

228c8941858f3df55305ea9e8b49dea732cc1b52
aekespong <andre.ekespong@gmail.com>
2023-11-30 00:04:08 +0100

    Rättat testfallen till singularis

--- tests/test_noq.py
+++ tests/test_noq.py
@@ -5,21 +5,21 @@ client = TestClient(app)
 
 
 def test_users():
-    response = client.get("/users")
+    response = client.get("/user")
     assert response.status_code == 200
     json_data = dict(response.json()[0])
     assert len(json_data["name"]) > 0
 
 
 def test_hosts():
-    response = client.get("/hosts")
+    response = client.get("/host")
     assert response.status_code == 200
     json_data = dict(response.json()[0])
     assert len(json_data["name"]) > 0
 
 
 def test_reservations():
-    response = client.get("/reservations")
+    response = client.get("/reservation")
     assert response.status_code == 200
     json_data = dict(response.json()[0])
     assert int(json_data["user_id"]) > 0

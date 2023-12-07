822e8d8c47862f4b6976de1a06a0c564df2c4505
Andre Ekespong <andre.ekespong@gmail.com>
2023-12-07 23:40:21 +0100

    Added endpoints for get, post and patch

--- api/users.py
+++ api/users.py
@@ -1,41 +1,50 @@
 from typing import Optional, List
 from datetime import datetime
-from fastapi import Depends, APIRouter
+from fastapi import Depends, APIRouter, HTTPException
 from pydantic import BaseModel
-from db.models.user import User
+from db.models.user import User, UserRead, UserCreate, UserPatch
 from db.db_setup import get_db, engine
 from sqlmodel import select, Session
 
 router = APIRouter()
 
-@router.get("/user", response_model=List[User])
-async def get_users(skip: int = 0, limit: int = 100):
+
+@router.get("/users", response_model=List[User])
+async def list_users(skip: int = 0, limit: int = 100):
     with Session(engine) as session:
         users = session.exec(select(User)).all()
         return users
 
 
-@router.post("/create_new_user",)
-async def create_new_user(user: User):
+@router.post("/users", response_model=UserRead)
+async def create_new_user(user: UserCreate):
     with Session(engine) as session:
-        session.add(user)
+        db_user: User = User.from_orm(user)
+        session.add(db_user)
         session.commit()
-    return user
+        session.refresh(db_user)
+    return db_user
 
 
-#@router.get("/user")
-#def get_users(skip: int = 0, limit: int = 100):
-#    with Session(engine) as session:
-#        user_list = session.query(User).offset(skip).limit(limit).all()
-#    return user_list
+@router.get("/users/{id}")
+async def get_user(id: int):
+    with Session(engine) as session:
+        user = session.get(User, id)
+        if not user:
+            raise HTTPException(status_code=404, detail="User not found")
+        return user
 
 
-# @router.post("/users")
-# async def create_user(user: UserBase):
-#     users.append(user)
-#     return "Success"
-
-
-# @router.get("/users/{id}")
-# async def get_user(id: int):
-#     return {"user": users[id]}
+@router.patch("/users/{id}", response_model=UserRead)
+def update_user(id: int, user: UserPatch):
+    with Session(engine) as session:
+        db_user = session.get(User, id)
+        if not db_user:
+            raise HTTPException(status_code=404, detail="User not found")
+        user_data = user.dict(exclude_unset=True)
+        for key, value in user_data.items():
+            setattr(db_user, key, value)
+        session.add(db_user)
+        session.commit()
+        session.refresh(db_user)
+        return db_user

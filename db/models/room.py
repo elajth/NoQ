7738b7b26f97573983fec082dff2d5011bbbbc05
Andre Ekespong <andre.ekespong@gmail.com>
2023-12-18 17:18:26 +0100

    Add room

---
+++ db/models/room.py
@@ -0,0 +1,20 @@
+from typing import Optional, List
+from sqlmodel import SQLModel, Field, Relationship
+from .common import DBCommon
+from .reservation import ReservationDB, Reservation_User
+
+
+class RoomType(DBCommon, table=True):
+    __table__ = "room_type"
+    type_name: str
+
+class RoomBase(SQLModel):
+    name: str
+    description: str
+    nbr_of_beds: int
+    room_type: RoomType
+
+class HostDB(RoomBase, DBCommon, table=True):
+    __tablename__ = "hosts"
+
+    #reservations: List["ReservationDB"] = Relationship(back_populates="host")

d3de7c2af0ccc6b42456cdf67042256104fdcc0d
Andre Ekespong <andre.ekespong@gmail.com>
2023-12-20 22:50:39 +0100

    Added read of ENV-variables

--- db/db_setup.py
+++ db/db_setup.py
@@ -3,17 +3,24 @@ from icecream import ic
 from sqlmodel import Session, create_engine
 from dotenv import load_dotenv
 
-# Read settings from .env file
-load_dotenv()
+# Read environment variables
+DATABASE_URL = os.environ.get("DATABASE_URL", "")
+DB_ECHO = os.environ.get("DB_ECHO", "")
+ic(DATABASE_URL)
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///noq.sqlite")
+if DATABASE_URL == "":
+    # Read settings from .env file
+    load_dotenv()
+
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///noq.sqlite")
+    DB_ECHO = bool(os.getenv("DB_ECHO", "False").lower() == "true")
+
 if "sqlite" in DATABASE_URL:
     connect_args = {"check_same_thread": False}
 else:
-    connect_args ={}
+    connect_args = {}
 
-DB_ECHO = bool(os.getenv("DB_ECHO", "False").lower() == "true")
-ic(DB_ECHO)
+ic(DATABASE_URL, DB_ECHO)
 engine = create_engine(
     DATABASE_URL, connect_args=connect_args, future=True, echo=DB_ECHO
 )

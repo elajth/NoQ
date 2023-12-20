38861942f2f65d203a05549eca26ae5065c61fba
Andre Ekespong <andre.ekespong@gmail.com>
2023-12-20 22:51:59 +0100

    Added tests for update

--- tests/test_noq.py
+++ tests/test_noq.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 from main import app
+from icecream import ic
 
 
 client = TestClient(app)
@@ -17,7 +18,7 @@ def test_users():
     # assert len(json_data["name"]) > 0
 
 
-def test_add_user():
+def test_add_and_update_user():
     person = {
         "name": "Victoria",
         "phone": "0708504033",
@@ -29,9 +30,39 @@ def test_add_user():
     user_id = response.json()["id"]
     assert user_id is not None and user_id > 0
 
+    # Update data
+    person = {"name": "Lisa"}
+    response = client.patch(f"/users/{user_id}", json=person)
+    assert response.status_code == 200
+    name = response.json()["name"]
+
+    assert user_id is not None and name == "Lisa"
+
 
 def test_hosts():
     response = client.get("/hosts")
     assert response.status_code == 200
     json_data = dict(response.json()[0])
     assert len(json_data["name"]) > 0
+
+
+def test_add_and_update_host():
+    host = {
+        "name": "Grimmans Akutboende",
+        "address1": "Aspgränd 30",
+        "address2": "19836 Sundsvall",
+        "count_of_available_places": 3,
+        "total_available_places": 3,
+    }
+    response = client.post("/hosts", json=host)
+    assert response.status_code == 200
+    host_id = response.json()["id"]
+    assert host_id is not None and host_id > 0
+
+    # Update data
+    host = {"name": "Stället"}
+    response = client.patch(f"/hosts/{host_id}", json=host)
+    assert response.status_code == 200
+    name = response.json()["name"]
+
+    assert host_id is not None and name == "Stället"
